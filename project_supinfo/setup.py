832985b824f50b4a0fee84b0806decf54ac37b27
Mathieuprt <mathieu.perrot06@gmail.com>
2025-04-09 15:17:41 +0200

    Suppresion des anciens dossiers via docker

--- project_supinfo/setup.py
+++ project_supinfo/setup.py
@@ -1,8 +1,8 @@
 from setuptools import find_packages, setup
 
 setup(
-    name="dagster_project",
-    packages=find_packages(exclude=["dagster_project_tests"]),
+    name="project_supinfo",
+    packages=find_packages(exclude=["project_supinfo_tests"]),
     install_requires=[
         "dagster",
         "dagster-cloud"
